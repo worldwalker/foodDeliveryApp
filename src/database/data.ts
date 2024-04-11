2ecbec286fa5eae0c04bf140603c77f794972c9d
worldwalker <mshf1993@gmail.com>
2024-04-11 20:33:35 +0900

    feat: add data for home screen

---
+++ src/database/data.ts
@@ -0,0 +1,50 @@
+export const categories = [
+  {
+    name: 'Appetizers',
+    image:
+      'https://insanelygoodrecipes.com/wp-content/uploads/2021/05/antipasto-skewers-with-basil-1.jpg',
+  },
+  {
+    name: 'Main Courses',
+    image:
+      'https://www.southernliving.com/thmb/XIHmbOagRrWxoOOrYnQih5VEV9A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2548301_QFSHe_646_0_0_0-fbd2a27e126942c8b33dbc3d696b0501.jpg',
+  },
+  {
+    name: 'Desserts',
+    image:
+      'https://prettysweetblog.com/wp-content/uploads/2021/01/No-bake-chocolate-hazelnut-dessert-in-a-glass-1-2.jpg',
+  },
+  {
+    name: 'Drinks',
+    image:
+      'https://hips.hearstapps.com/hmg-prod/images/sex-on-the-beach-vertical-6442f7c22a415.jpg',
+  },
+];
+
+export const featuredDishes = [
+  {
+    name: 'Kebab',
+    image:
+      'https://cookingorgeous.com/wp-content/uploads/2021/06/lamb-shish-kebab-20.jpg',
+  },
+  {
+    name: 'Lavash',
+    image:
+      'https://cdn.foodaciously.com/static/recipes/6143e5d7-4d19-4933-9fe4-3e1589156a83/lavash-wrap-recipe-1cbaf8fb9032f4192afb8a0800db0601-2560-q60.jpg',
+  },
+  {
+    name: 'Doner',
+    image:
+      'https://www.thespruceeats.com/thmb/ao7czXh1afSBamtQnDUilsLD3uc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/doner-kebab-recipe-4171703-hero-01-c14d8b7189df4685a50bae311317f831.jpg',
+  },
+  {
+    name: 'Kebab',
+    image:
+      'https://cookingorgeous.com/wp-content/uploads/2021/06/lamb-shish-kebab-20.jpg',
+  },
+  {
+    name: 'Lavash',
+    image:
+      'https://cdn.foodaciously.com/static/recipes/6143e5d7-4d19-4933-9fe4-3e1589156a83/lavash-wrap-recipe-1cbaf8fb9032f4192afb8a0800db0601-2560-q60.jpg',
+  },
+];
